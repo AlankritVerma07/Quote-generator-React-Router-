418fd0e3d584afbf7f009dddf4ffbca2fbad4318
Alankrit Verma <alankritverma.lko@gmail.com>
2021-09-29 12:42:20 -0700

    Quote generator using router and custom hooks

---
+++ src/components/comments/Comments.js
@@ -0,0 +1,60 @@
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
+import CommentsList from "./CommentsList";
+import LoadingSpinner from "../UI/LoadingSpinner";
+
+const Comments = () => {
+  const [isAddingComment, setIsAddingComment] = useState(false);
+  const params = useParams();
+  const { quoteId } = params;
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+  const startAddCommentHandler = () => {
+    setIsAddingComment(true);
+  };
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]); //After the comment is added, we want to automatically reload the current page to display all the comments
+  let comments;
+
+  if (status === "pending") {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === "completed" && loadedComments && loadedComments.length > 0) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (status === "completed" && (!loadedComments || loadedComments.length === 0)) {
+    comments = <p className="centered">No comments were added yet!</p>;
+  }
+  return (
+    <section className={classes.comments}>
+      <h2>User Comments</h2>
+      {!isAddingComment && (
+        <button className="btn" onClick={startAddCommentHandler}>
+          Add a Comment
+        </button>
+      )}
+      {isAddingComment && (
+        <NewCommentForm onAddComment={addedCommentHandler} quoteId={quoteId} />
+      )}
+      {comments}
+    </section>
+  );
+};
+
+export default Comments;
